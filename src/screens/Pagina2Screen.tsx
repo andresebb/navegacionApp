876b7460e50696dd2d5ab20c06ded042bf60bb92
Andres Betancourt <aebb001@gmail.com>
2021-06-11 15:30:47 -0500

    Instalando dependencias y config inicial de react navigation

---
+++ src/screens/Pagina2Screen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const Pagina2Screen = () => {
+  return <Text>Pagina 2 Screen</Text>;
+};
---
+++ src/screens/Pagina3Screen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const Pagina3Screen = () => {
+  return <Text>Pagina 3 Screen</Text>;
+};
