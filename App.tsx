7291d3c002e2f5894150a5dd27874f49f3db2f40
Andres Betancourt <aebb001@gmail.com>
2021-06-14 19:46:50 -0500

    Configuracion inicial del Drawer Navigation

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './src/navigator/StackNavigator';
+import {MenuLateralBasico} from './src/navigator/MenuLateralBasico';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <MenuLateralBasico />
     </NavigationContainer>
   );
 };
