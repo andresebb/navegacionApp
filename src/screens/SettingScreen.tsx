204b27a916d2e3b5bedef5ac6ad5be7aeffb476e
Andres Betancourt <aebb001@gmail.com>
2021-06-14 21:02:00 -0500

    Terminando Drawe

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../theme/appTheme';
 
 export const SettingScreen = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Soy la settings Screen</Text>
+    <View
+      style={{
+        marginTop: insets.top + 20,
+      }}>
+      <Text style={styles.title}>Settings Screen</Text>
     </View>
   );
 };
