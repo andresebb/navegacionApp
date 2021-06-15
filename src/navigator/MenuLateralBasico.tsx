4e522809a4d0fe658c763e71fec39e2b41ea8f78
Andres Betancourt <aebb001@gmail.com>
2021-06-14 20:08:02 -0500

    Menu Lateral Basico, Menu de hamburguesa

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {SettingScreen} from '../screens/SettingScreen';
 import {StackNavigator} from './StackNavigator';
-
+import {useWindowDimensions} from 'react-native';
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
+  const dimensions = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerType={dimensions.width >= 768 ? 'permanent' : 'front'}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="SettingScreen" component={SettingScreen} />
     </Drawer.Navigator>
--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,13 +1,23 @@
+import React, {useEffect} from 'react';
+import {DrawerScreenProps} from '@react-navigation/drawer';
 import {StackScreenProps} from '@react-navigation/stack';
-import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {styles} from '../theme/appTheme';
 
 // Las props nos llegan debido al stackNavigator que creamos
-interface Props extends StackScreenProps<any, any> {}
+// interface Props extends StackScreenProps<any, any> {}
+interface Props extends DrawerScreenProps<any, any> {}
 
 export const Pagina1Screen = ({navigation}: Props) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
+      ),
+    });
+  }, []);
+
   return (
     <View>
       <Text style={styles.title}>Pagina Screen 1</Text>
